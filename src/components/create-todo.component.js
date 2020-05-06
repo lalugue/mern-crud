1fd0b315483b922d25cc1492b61c95619aadc128
angelo.lugue@eee.upd.edu.ph <m@sterl3arner>
2020-05-06 20:13:21 +0800

    add radio buttons for priority

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -76,6 +76,38 @@ export default class CreateTodo extends Component{
                         onChange={this.onChangeTodoResponsible}
                         />
                     </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityLow"
+                                value="Low"
+                                checked={this.state.todo_priority==='Low'}
+                                onChange={this.onChangeTodoPriority} />
+                                <label className="form-check-label">Low</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityMedium"
+                                value="Medium"
+                                checked={this.state.todo_priority==='Medium'}
+                                onChange={this.onChangeTodoPriority} />
+                                <label className="form-check-label">Medium</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityHigh"
+                                value="High"
+                                checked={this.state.todo_priority==='High'}
+                                onChange={this.onChangeTodoPriority} />
+                                <label className="form-check-label">High</label>
+                            </div>
+                    </div>
                 </form>
             </div>
         )
