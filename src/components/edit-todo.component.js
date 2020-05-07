918475be563bbf95f20227b603b4b8ffe9d4c6ac
lalugue <angelo.lugue@eee.upd.edu.ph>
2020-05-07 23:12:04 +0800

    add onComponentDidMount behavior

--- src/components/edit-todo.component.js
+++ src/components/edit-todo.component.js
@@ -15,6 +15,26 @@ export default class EditTodo extends Component{
 
     }
 
+    //when a component is loaded, do this
+    componentDidMount(){
+        console.log('this is the props for editing: ')
+        console.log(this.props)
+        //the specific props location can be seen in developer console
+        axios.get('http://localhost:4000/todos/'+this.props.match.params.id)
+            .then(res=>{
+                this.setState({
+                    todo_description: res.data.todo_description,
+                    todo_responsible: res.data.todo_responsible,
+                    todo_priority: res.data.todo_priority,
+                    todo_completed: res.data.todo_completed
+                })
+            })
+            .catch(err=>{
+                console.log("error detected while fetching props for update:")
+                console.log(err)
+            })
+    }
+
     render(){
         return(
             <div>
