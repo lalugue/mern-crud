cfab6fba6080f82e47c52ce3538e29c185609d7c
lalugue <angelo.lugue@eee.upd.edu.ph>
2020-05-07 11:57:35 +0800

    fetch todos from backend via componentDidMount()

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 //export a single class (default export)
 export default class TodosList extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {todos: []};
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:4000/todos')
+            .then(res => {
+                this.setState({todos: res.data})
+            })
+            .catch(err => {
+                console.log('an error occurred while fetching')
+                console.log(err)
+            })
+    }
+
     render(){
         return(
             <div>
-            <p>Welcome to the TodosList component!</p>
+            <h3>Todos List</h3>
+            <table className="table table-striped" style={{marginTop:20}}>
+                
+            </table>
             </div>
         )
     }
