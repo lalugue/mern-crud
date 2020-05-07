10646652138de735a2e3f39b55d913d6ee58410b
lalugue <angelo.lugue@eee.upd.edu.ph>
2020-05-07 11:56:29 +0800

    add axios to create-todo

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 //export a single class (default export)
 export default class CreateTodo extends Component{
@@ -46,6 +47,16 @@ export default class CreateTodo extends Component{
         console.log(`Todo Description: ${this.state.todo_description}`)
         console.log(`Todo Resposible: ${this.state.todo_responsible}`)
         console.log(`Todo Priority: ${this.state.todo_priority}`)
+
+        const newTodo = {
+            todo_description : this.state.todo_description,
+            todo_responsible : this.state.todo_responsible,
+            todo_priority : this.state.todo_priority,
+            todo_completed : this.state.todo_completed
+        }
+
+        axios.post('http://localhost:4000/todos/add',newTodo)
+             .then(res => console.log(res.data))
 
         //clean up form after submission
         this.setState({
